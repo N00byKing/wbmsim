06f73cab7064d90b8c6f01231f245b8a2a32cde7
Mark-Weston <markweston@cock.li>
2019-01-19 14:00:36 +0200

    Finally, return if wire is valid

--- src/main.c
+++ src/main.c
@@ -10,8 +10,64 @@
 #define SM 0.9 // Size Multiplier to avoid false-positive collisions
 
 static bool isValidWire(const char *w);
+static void lbatch(const char *w, size_t *i, double *v);
 static void lbatchLine(double x, double y, double a, double l, double t, size_t *ni, size_t *nv, size_t *i, double *v);
 static void lbatchRingSlice(double x, double y, double r, double t, double a, double o, size_t *ni, size_t *nv, size_t *i, double *v);
+static void lbatchCircle(double x, double y, double r, size_t *ni, size_t *nv, size_t *i, double *v);
+static bool segmentsCollide(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3);
+static void rayCollision(double ax, double ay, double aa, double bx, double by, double ba, double *al, double *bl);
+
+//////////////////////////////////////////////////////////////////////////////
+#include "../lib/lib.h"
+#include <GLFW/glfw3.h>
+static void debug(size_t ni, const size_t *i, const double *v) {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_SAMPLES, 0);
+
+    GLFWmonitor *mon = glfwGetPrimaryMonitor();
+    const GLFWvidmode *vm = glfwGetVideoMode(mon);
+    glfwWindowHint(GLFW_RED_BITS, vm->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, vm->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, vm->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, vm->refreshRate);
+
+    GLFWwindow *win = glfwCreateWindow(vm->width, vm->height, "title", mon, NULL);
+    glfwMakeContextCurrent(win);
+    glfwSwapInterval(1);
+
+    rInit();
+    Batch b = batchNew();
+    for (size_t j = 0; j < ni; j += 2) {
+        double x0 = v[i[j + 0] * 2 + 0];
+        double y0 = v[i[j + 0] * 2 + 1];
+        double x1 = v[i[j + 1] * 2 + 0];
+        double y1 = v[i[j + 1] * 2 + 1];
+        double a = atan2(y1 - y0, x1 - x0);
+        double l = sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+        batchLine(&b, x0, y0, a, l, 0.02, (const uint8_t[]){255,255,255});
+    }
+
+    while (!glfwWindowShouldClose(win)) {
+        glfwPollEvents();
+        int winW, winH;
+        glfwGetFramebufferSize(win, &winW, &winH);
+        rViewport(0, 0, winW, winH);
+        float ar = (float)winW/(float)winH;
+        rPipe(1.0 / ar / 4.0, 1.0 / 4.0, 0, 0);
+        rClear(0,0,0);
+        rTris(b.ni, b.i, b.v);
+        glfwSwapBuffers(win);
+    }
+
+    batchDel(&b);
+    rExit();
+    glfwTerminate();
+}
+//////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv) {
     if (argc != 2 || strspn(argv[1], "RUD") != strlen(argv[1])) return 42;
@@ -20,8 +76,8 @@ int main(int argc, char **argv) {
 
 static bool isValidWire(const char *w) {
     size_t l = strlen(w);
-    size_t ni = 0;
-    size_t nv = 0;
+    size_t ni = 8 + QC * 4;
+    size_t nv = 4 + QC * 2;
 
     for (size_t i = 0; i < l; ++i) {
         ni += w[i] == 'R' ? 8 : QQ * 4;
@@ -31,10 +87,42 @@ static bool isValidWire(const char *w) {
     size_t *i = malloc(ni * sizeof(*i));
     double *v = malloc(2 * nv * sizeof(*v));
 
+    lbatch(w, i, v);
+
+//    debug(ni, i, v);
+
+    size_t ioffset = 0;
+    for (size_t j = l - 1; j < l; --j) {
+        size_t prevIoffset = ioffset;
+        ioffset += w[j] == 'R' ? 8 : 4;
+        for (size_t g = ioffset; g < ni; g += 2) {
+            for (size_t h = prevIoffset; h < ioffset; h += 2) {
+                double x0 = v[i[h + 0] * 2 + 0];
+                double y0 = v[i[h + 0] * 2 + 1];
+                double x1 = v[i[h + 1] * 2 + 0];
+                double y1 = v[i[h + 1] * 2 + 1];
+                double x2 = v[i[g + 0] * 2 + 0];
+                double y2 = v[i[g + 0] * 2 + 1];
+                double x3 = v[i[g + 1] * 2 + 0];
+                double y3 = v[i[g + 1] * 2 + 1];
+                if (segmentsCollide(x0, y0, x1, y1, x2, y2, x3, y3)) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+static void lbatch(const char *w, size_t *i, double *v) {
+    size_t l = strlen(w);
     double x = 0;
     double y = 0;
+    size_t ni = 0;
+    size_t nv = 0;
     char dir = 'R';
-    ni = nv = 0;
+
     for (size_t j = l - 1; j < l; --j) {
         if (dir == 'U') {
             if (w[j] == 'U') {
@@ -99,9 +187,9 @@ static bool isValidWire(const char *w) {
         }
     }
 
-    // TODO: collision detection
-
-    return true;
+    lbatchLine(-99, 0, 0, 99 - 1 + SM, SM, &ni, &nv, i, v);
+    lbatchCircle(0,  1, 0.5 * SM, &ni, &nv, i, v);
+    lbatchCircle(0, -1, 0.5 * SM, &ni, &nv, i, v);
 }
 
 static void lbatchLine(double x, double y, double a, double l, double t, size_t *ni, size_t *nv, size_t *i, double *v) {
@@ -156,4 +244,51 @@ static void lbatchRingSlice(double x, double y, double r, double t, double o, do
 
     *ni += QQ * 4;
     *nv += QQ * 2;
+}
+
+static void lbatchCircle(double x, double y, double r, size_t *ni, size_t *nv, size_t *i, double *v) {
+    for (size_t j = 0; j < QC - 1; ++j) {
+        i[*ni + j * 2 + 0] = *nv + j + 0;
+        i[*ni + j * 2 + 1] = *nv + j + 1;
+    }
+    i[*ni + QC * 2 - 2] = *nv + QC - 1;
+    i[*ni + QC * 2 - 1] = *nv;
+
+    double da = PI * 2 / QC;
+    for (size_t j = 0; j < QC; ++j) {
+        double angle = da * j;
+        double s = sin(angle);
+        double c = cos(angle);
+        v[*nv * 2 + j * 2 + 0] = x + c * r;
+        v[*nv * 2 + j * 2 + 1] = y + s * r;
+    }
+
+    *ni += QC * 2;
+    *nv += QC;
+}
+
+static bool segmentsCollide(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3) {
+    double aa = atan2(y1 - y0, x1 - x0);
+    double al = sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+    double ba = atan2(y3 - y2, x3 - x2);
+    double bl = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+    double aL, bL;
+    rayCollision(x0, y0, aa, x2, y2, ba, &aL, &bL);
+    return !(isnan(aL) || isnan(bL) || aL < 0 || bL < 0 || aL > al || bL > bl);
+}
+
+static void rayCollision(double ax, double ay, double aa, double bx, double by, double ba, double *al, double *bl) {
+    double ac = cosf(aa);
+    double bc = cosf(ba);
+    double as = sinf(aa);
+    double bs = sinf(ba);
+    double dx = bx - ax;
+    double dy = by - ay;
+    double det = bc * as - ac * bs;
+    if (det == 0) {
+        *al = *bl = NAN;
+    } else {
+        *al = (dy * bc - dx * bs) / det;
+        *bl = (dy * ac - dx * as) / det;
+    }
 }
