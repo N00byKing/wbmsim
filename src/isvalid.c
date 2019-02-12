2f5751e9c72426e9cdf29a2826bcb394b8b8c1e9
Mark-Weston <markweston@cock.li>
2019-02-12 05:22:58 +0200

    Speed up isValidWire() by orders of magnitude

--- src/isvalid.c
+++ src/isvalid.c
@@ -3,232 +3,345 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "../lib/lib.h"
-#include "main.h"
+#define PI 3.1415926535
+#define SM 0.99
+#define ZERO_TOLERANCE 0.0009765625
 
-#define SM 0.9 // Size Multiplier
-#define WL 99 // static Wire Length
+typedef enum {
+    CURVE_TYPE_LINE,
+    CURVE_TYPE_ARC
+} CurveType;
 
-static void lbatch(const char *w, size_t *i, double *v);
-static void lbatchLine(double x, double y, double a, double l, double t, size_t *ni, size_t *nv, size_t *i, double *v);
-static void lbatchRingSlice(double x, double y, double r, double t, double a, double o, size_t *ni, size_t *nv, size_t *i, double *v);
-static void lbatchCircle(double x, double y, double r, size_t *ni, size_t *nv, size_t *i, double *v);
-static bool segmentsCollide(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3);
-static void rayCollision(double ax, double ay, double aa, double bx, double by, double ba, double *al, double *bl);
+typedef struct {
+    double x, y, a, l;
+} Line;
+
+typedef struct {
+    double x, y, r, o, a;
+} Arc;
+
+typedef struct {
+    double x, y, r;
+} Circle;
+
+typedef struct {
+    CurveType type;
+    union {
+        Line line;
+        Arc arc;
+    } curve;
+} Curve;
+
+bool isValidWire(const char *w);
+static Curve *buildCurve(const char *w);
+static void buildCurvePart(char w, double *x, double *y, char *dir, Curve *c);
+static void buildLine(Curve *c, Line line, double t);
+static void buildArc(Curve *c, Arc arc, double t);
+static bool detectCollision(size_t l, const Curve *c);
+static bool detectCurveCollision(Curve a, Curve b);
+static bool collLineLine(Line a, Line b);
+static bool collLineArc(Line a, Arc b);
+static bool collArcArc(Arc a, Arc b);
+static size_t collCircleCircle(Circle a, Circle b, double *x, double *y);
+static double s(double x);
+static double ctg(double a);
+static double len(double x1, double y1, double x2, double y2);
 
 bool isValidWire(const char *w) {
-    size_t l = strlen(w);
-    size_t ni = 8 + QC * 4;
-    size_t nv = 4 + QC * 2;
+    Curve *c = buildCurve(w);
+    bool collision = detectCollision(strlen(w), c);
+    free(c);
+    return !collision;
+}
 
-    for (size_t i = 0; i < l; ++i) {
-        ni += w[i] == 'R' ? 8 : QQ * 4;
-        nv += w[i] == 'R' ? 4 : QQ * 2;
+static Curve *buildCurve(const char *w) {
+    size_t l = strlen(w);
+    double x[2] = {0, 0};
+    double y[2] = {0, 0};
+    char dir = 'R';
+    Curve *c = malloc(l * 4 * sizeof(*c));
+
+    for (size_t i = l - 1; i < l; --i) {
+        size_t j = l - i - 1;
+        buildCurvePart(w[i], x, y, &dir, c + j * 4);
     }
 
-    size_t *i = malloc(ni * sizeof(*i));
-    double *v = malloc(2 * nv * sizeof(*v));
+    return c;
+}
 
-    lbatch(w, i, v);
+static void buildCurvePart(char w, double *x, double *y, char *dir, Curve *c) {
+    double a = PI / 2 * SM;
+    if (*dir == 'U') {
+        if (w == 'U') {
+            buildArc(c, (Arc){*x - 1, *y, 1, 0, a}, SM);
+            *x -= 1;
+            *y += 1;
+            *dir = 'L';
+        } else if (w == 'D') {
+            buildArc(c, (Arc){*x + 1, *y, 1, PI - a, a}, SM);
+            *x += 1;
+            *y += 1;
+            *dir = 'R';
+        } else {
+            buildLine(c, (Line){*x, *y, PI / 2, a}, SM);
+            *y += PI / 2;
+        }
+    } else if (*dir == 'D') {
+        if (w == 'U') {
+            buildArc(c, (Arc){*x + 1, *y, 1, PI, a}, SM);
+            *x += 1;
+            *y -= 1;
+            *dir = 'R';
+        } else if (w == 'D') {
+            buildArc(c, (Arc){*x - 1, *y, 1, PI * 2 - a, a}, SM);
+            *x -= 1;
+            *y -= 1;
+            *dir = 'L';
+        } else {
+            buildLine(c, (Line){*x, *y, PI * 3 / 2, a}, SM);
+            *y -= PI / 2;
+        }
+    } else if (*dir == 'L') {
+        if (w == 'U') {
+            buildArc(c, (Arc){*x, *y - 1, 1, PI / 2, a}, SM);
+            *x -= 1;
+            *y -= 1;
+            *dir = 'D';
+        } else if (w == 'D') {
+            buildArc(c, (Arc){*x, *y + 1, 1, PI * 3 / 2 - a, a}, SM);
+            *x -= 1;
+            *y += 1;
+            *dir = 'U';
+        } else {
+            buildLine(c, (Line){*x, *y, PI, a}, SM);
+            *x -= PI / 2;
+        }
+    } else {
+        if (w == 'U') {
+            buildArc(c, (Arc){*x, *y + 1, 1, PI * 3 / 2, a}, SM);
+            *x += 1;
+            *y += 1;
+            *dir = 'U';
+        } else if (w == 'D') {
+            buildArc(c, (Arc){*x, *y - 1, 1, PI / 2 - a, a}, SM);
+            *x += 1;
+            *y -= 1;
+            *dir = 'D';
+        } else {
+            buildLine(c, (Line){*x, *y, 0, a}, SM);
+            *x += PI / 2;
+        }
+    }
+}
 
-    size_t ioffset = 0;
-    for (size_t j = l - 1; j < l; --j) {
-        size_t prevIoffset = ioffset;
-        ioffset += w[j] == 'R' ? 8 : QQ * 4;
-        for (size_t g = ioffset; g < ni; g += 2) {
-            for (size_t h = prevIoffset; h < ioffset; h += 2) {
-                double x0 = v[i[h + 0] * 2 + 0];
-                double y0 = v[i[h + 0] * 2 + 1];
-                double x1 = v[i[h + 1] * 2 + 0];
-                double y1 = v[i[h + 1] * 2 + 1];
-                double x2 = v[i[g + 0] * 2 + 0];
-                double y2 = v[i[g + 0] * 2 + 1];
-                double x3 = v[i[g + 1] * 2 + 0];
-                double y3 = v[i[g + 1] * 2 + 1];
-                if (segmentsCollide(x0, y0, x1, y1, x2, y2, x3, y3)) {
-                    return false;
+static void buildLine(Curve *c, Line line, double t) {
+    double x1 = line.x + cos(line.a - PI / 2) * t / 2;
+    double y1 = line.y + sin(line.a - PI / 2) * t / 2;
+    double x2 = line.x + cos(line.a + PI / 2) * t / 2;
+    double y2 = line.y + sin(line.a + PI / 2) * t / 2;
+    double x3 = x1 + cos(line.a) * line.l;
+    double y3 = y1 + sin(line.a) * line.l;
+    c[0] = (Curve){CURVE_TYPE_LINE, .curve.line = {x1, y1, line.a, line.l}};
+    c[1] = (Curve){CURVE_TYPE_LINE, .curve.line = {x2, y2, line.a, line.l}};
+    c[2] = (Curve){CURVE_TYPE_LINE, .curve.line = {x1, y1, line.a + PI / 2, t}};
+    c[3] = (Curve){CURVE_TYPE_LINE, .curve.line = {x3, y3, line.a + PI / 2, t}};
+}
+
+static void buildArc(Curve *c, Arc arc, double t) {
+    c[0] = (Curve){CURVE_TYPE_LINE, .curve.line = {arc.x + cos(arc.o) * (arc.r - t / 2), arc.y + sin(arc.o) * (arc.r - t / 2), arc.o, t}};
+    c[1] = (Curve){CURVE_TYPE_LINE, .curve.line = {arc.x + cos(arc.o + arc.a) * (arc.r - t / 2), arc.y + sin(arc.o + arc.a) * (arc.r - t / 2), arc.o + arc.a, t}};
+    c[2] = (Curve){CURVE_TYPE_ARC, .curve.arc = {arc.x, arc.y, arc.r - t / 2, arc.o, arc.a}};
+    c[3] = (Curve){CURVE_TYPE_ARC, .curve.arc = {arc.x, arc.y, arc.r + t / 2, arc.o, arc.a}};
+}
+
+static bool detectCollision(size_t l, const Curve *c) {
+    for (size_t i = 0; i < l; ++i) {
+        for (size_t j = i + 1; j < l; ++j) {
+            for (size_t k = 0; k < 4; ++k) {
+                for (size_t g = 0; g < 4; ++g) {
+                    if (detectCurveCollision(c[i * 4 + k], c[j * 4 + g])) {
+                        return true;
+                    }
                 }
             }
         }
     }
 
-    return true;
-}
+    Curve x[6];
+    x[0] = (Curve){CURVE_TYPE_ARC, .curve.arc = {0,  1, SM / 2, 0, PI * 2 * SM}};
+    x[1] = (Curve){CURVE_TYPE_ARC, .curve.arc = {0, -1, SM / 2, 0, PI * 2 * SM}};
+    x[2] = (Curve){CURVE_TYPE_LINE, .curve.line = {-99, -SM / 2, 0, 98 + SM}};
+    x[3] = (Curve){CURVE_TYPE_LINE, .curve.line = {-99,  SM / 2, 0, 98 + SM}};
+    x[4] = (Curve){CURVE_TYPE_LINE, .curve.line = {-99, -SM / 2, PI / 2, SM}};
+    x[5] = (Curve){CURVE_TYPE_LINE, .curve.line = {SM - 1, -SM / 2, PI / 2, SM}};
 
-static void lbatch(const char *w, size_t *i, double *v) {
-    size_t l = strlen(w);
-    double x = 0;
-    double y = 0;
-    size_t ni = 0;
-    size_t nv = 0;
-    char dir = 'R';
-
-    for (size_t j = l - 1; j < l; --j) {
-        if (dir == 'U') {
-            if (w[j] == 'U') {
-                lbatchRingSlice(x - 1, y, 1, SM, 0, SM * PI / 2, &ni, &nv, i, v);
-                x -= 1;
-                y += 1;
-                dir = 'L';
-            } else if (w[j] == 'D') {
-                lbatchRingSlice(x + 1, y, 1, SM, PI, SM * -PI / 2, &ni, &nv, i, v);
-                x += 1;
-                y += 1;
-                dir = 'R';
-            } else {
-                lbatchLine(x, y, PI / 2, SM * PI / 2, SM, &ni, &nv, i, v);
-                y += PI / 2;
-            }
-        } else if (dir == 'D') {
-            if (w[j] == 'U') {
-                lbatchRingSlice(x + 1, y, 1, SM, PI, SM * PI / 2, &ni, &nv, i, v);
-                x += 1;
-                y -= 1;
-                dir = 'R';
-            } else if (w[j] == 'D') {
-                lbatchRingSlice(x - 1, y, 1, SM, 0, SM * -PI / 2, &ni, &nv, i, v);
-                x -= 1;
-                y -= 1;
-                dir = 'L';
-            } else {
-                lbatchLine(x, y, -PI / 2, SM * PI / 2, SM, &ni, &nv, i, v);
-                y -= PI / 2;
-            }
-        } else if (dir == 'L') {
-            if (w[j] == 'U') {
-                lbatchRingSlice(x, y - 1, 1, SM, PI / 2, SM * PI / 2, &ni, &nv, i, v);
-                x -= 1;
-                y -= 1;
-                dir = 'D';
-            } else if (w[j] == 'D') {
-                lbatchRingSlice(x, y + 1, 1, SM, -PI / 2, SM * -PI / 2, &ni, &nv, i, v);
-                x -= 1;
-                y += 1;
-                dir = 'U';
-            } else {
-                lbatchLine(x, y, 0, SM * -PI / 2, SM, &ni, &nv, i, v);
-                x -= PI / 2;
-            }
-        } else {
-            if (w[j] == 'U') {
-                lbatchRingSlice(x, y + 1, 1, SM, -PI / 2, SM * PI / 2, &ni, &nv, i, v);
-                x += 1;
-                y += 1;
-                dir = 'U';
-            } else if (w[j] == 'D') {
-                lbatchRingSlice(x, y - 1, 1, SM, PI / 2, SM * -PI / 2, &ni, &nv, i, v);
-                x += 1;
-                y -= 1;
-                dir = 'D';
-            } else {
-                lbatchLine(x, y, 0, SM * PI / 2, SM, &ni, &nv, i, v);
-                x += PI / 2;
+    for (size_t i = 0; i < l * 4; ++i) {
+        for (size_t j = 0; j < 6; ++j) {
+            if (detectCurveCollision(c[i], x[j])) {
+                return true;
             }
         }
     }
 
-    lbatchLine(-WL, 0, 0, WL - 1 + SM, SM, &ni, &nv, i, v);
-    lbatchCircle(0,  1, 0.5 * SM, &ni, &nv, i, v);
-    lbatchCircle(0, -1, 0.5 * SM, &ni, &nv, i, v);
+    return false;
 }
 
-static void lbatchLine(double x, double y, double a, double l, double t, size_t *ni, size_t *nv, size_t *i, double *v) {
-    i[*ni + 0] = *nv + 0;
-    i[*ni + 1] = *nv + 1;
-    i[*ni + 2] = *nv + 1;
-    i[*ni + 3] = *nv + 2;
-    i[*ni + 4] = *nv + 2;
-    i[*ni + 5] = *nv + 3;
-    i[*ni + 6] = *nv + 3;
-    i[*ni + 7] = *nv + 0;
-
-    double dx = sin(a) * t / 2;
-    double dy = cos(a) * t / 2;
-    double X = x + cos(a) * l;
-    double Y = y + sin(a) * l;
-    v[*nv * 2 + 0] = x - dx;
-    v[*nv * 2 + 1] = y + dy;
-    v[*nv * 2 + 2] = x + dx;
-    v[*nv * 2 + 3] = y - dy;
-    v[*nv * 2 + 4] = X + dx;
-    v[*nv * 2 + 5] = Y - dy;
-    v[*nv * 2 + 6] = X - dx;
-    v[*nv * 2 + 7] = Y + dy;
-
-    *ni += 8;
-    *nv += 4;
-}
-
-static void lbatchRingSlice(double x, double y, double r, double t, double o, double a, size_t *ni, size_t *nv, size_t *i, double *v) {
-    for (size_t j = 0; j < QQ - 1; ++j) {
-        i[*ni + j * 4 + 0] = *nv + j * 2 + 0;
-        i[*ni + j * 4 + 1] = *nv + j * 2 + 2;
-        i[*ni + j * 4 + 2] = *nv + j * 2 + 1;
-        i[*ni + j * 4 + 3] = *nv + j * 2 + 3;
-    }
-    i[*ni + QQ * 4 - 4] = *nv + 0;
-    i[*ni + QQ * 4 - 3] = *nv + 1;
-    i[*ni + QQ * 4 - 2] = *nv + QQ * 2 - 2;
-    i[*ni + QQ * 4 - 1] = *nv + QQ * 2 - 1;
-
-    double da = a / (QQ - 1);
-    for (size_t j = 0; j < QQ; ++j) {
-        double angle = o + da * j;
-        double s = sin(angle);
-        double c = cos(angle);
-        v[*nv * 2 + j * 4 + 0] = x + c * (r - t / 2);
-        v[*nv * 2 + j * 4 + 1] = y + s * (r - t / 2);
-        v[*nv * 2 + j * 4 + 2] = x + c * (r + t / 2);
-        v[*nv * 2 + j * 4 + 3] = y + s * (r + t / 2);
-    }
-
-    *ni += QQ * 4;
-    *nv += QQ * 2;
-}
-
-static void lbatchCircle(double x, double y, double r, size_t *ni, size_t *nv, size_t *i, double *v) {
-    for (size_t j = 0; j < QC - 1; ++j) {
-        i[*ni + j * 2 + 0] = *nv + j + 0;
-        i[*ni + j * 2 + 1] = *nv + j + 1;
-    }
-    i[*ni + QC * 2 - 2] = *nv + QC - 1;
-    i[*ni + QC * 2 - 1] = *nv;
-
-    double da = PI * 2 / QC;
-    for (size_t j = 0; j < QC; ++j) {
-        double angle = da * j;
-        double s = sin(angle);
-        double c = cos(angle);
-        v[*nv * 2 + j * 2 + 0] = x + c * r;
-        v[*nv * 2 + j * 2 + 1] = y + s * r;
-    }
-
-    *ni += QC * 2;
-    *nv += QC;
-}
-
-static bool segmentsCollide(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3) {
-    double aa = atan2(y1 - y0, x1 - x0);
-    double al = sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
-    double ba = atan2(y3 - y2, x3 - x2);
-    double bl = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
-    double aL, bL;
-    rayCollision(x0, y0, aa, x2, y2, ba, &aL, &bL);
-    return !(isnan(aL) || isnan(bL) || aL < 0 || bL < 0 || aL > al || bL > bl);
-}
-
-static void rayCollision(double ax, double ay, double aa, double bx, double by, double ba, double *al, double *bl) {
-    double ac = cosf(aa);
-    double bc = cosf(ba);
-    double as = sinf(aa);
-    double bs = sinf(ba);
-    double dx = bx - ax;
-    double dy = by - ay;
-    double det = bc * as - ac * bs;
-    if (det == 0) {
-        *al = *bl = NAN;
+static bool detectCurveCollision(Curve a, Curve b) {
+    if (a.type == CURVE_TYPE_ARC) {
+        if (b.type == CURVE_TYPE_ARC) {
+            if (collArcArc(a.curve.arc, b.curve.arc)) {
+                return true;
+            }
+        } else {
+            if (collLineArc(b.curve.line, a.curve.arc)) {
+                return true;
+            }
+        }
     } else {
-        *al = (dy * bc - dx * bs) / det;
-        *bl = (dy * ac - dx * as) / det;
+        if (b.type == CURVE_TYPE_ARC) {
+            if (collLineArc(a.curve.line, b.curve.arc)) {
+                return true;
+            }
+        } else {
+            if (collLineLine(a.curve.line, b.curve.line)) {
+                return true;
+            }
+        }
     }
+    return false;
+}
+
+static bool collLineLine(Line a, Line b) {
+    double l1, l2;
+    if (fabs(a.a - b.a) < ZERO_TOLERANCE) {
+        double l = len(a.x, a.y, b.x, b.y);
+        if (l > a.l && l > b.l) {
+            return false;
+        }
+        double x1 = a.x + cos(a.a) * l;
+        double y1 = a.y + sin(a.a) * l;
+        double x2 = b.x + cos(b.a) * l;
+        double y2 = b.y + sin(b.a) * l;
+        bool c1 = (fabs(x1 - b.x) < ZERO_TOLERANCE) && (fabs(y1 - b.y) < ZERO_TOLERANCE);
+        bool c2 = (fabs(x2 - a.x) < ZERO_TOLERANCE) && (fabs(y2 - a.y) < ZERO_TOLERANCE);
+        return c1 || c2;
+    } else if (fabs(cos(a.a)) > fabs(sin(a.a))) {
+        l2 = (a.y - b.y + tan(a.a) * (b.x - a.x)) / (sin(b.a) - tan(a.a) * cos(b.a));
+        l1 = (b.x - a.x + cos(b.a) * l2) / cos(a.a);
+    } else {
+        l2 = (a.x - b.x + ctg(a.a) * (b.y - a.y)) / (cos(b.a) - ctg(a.a) * sin(b.a));
+        l1 = (b.y - a.y + sin(b.a) * l2) / sin(a.a);
+    }
+    return l1 >= 0 && l1 <= a.l && l2 >= 0 && l2 <= b.l;
+}
+
+static bool collLineArc(Line a, Arc b) {
+
+    double A = 1;
+    double B = 2 * (cos(a.a) * (a.x - b.x) + sin(a.a) * (a.y - b.y));
+    double C = s(a.x) - 2 * a.x * b.x + s(b.x) + s(a.y) - 2 * a.y * b.y + s(b.y) - s(b.r);
+    double D = s(B) - 4 * A * C;
+    size_t n = 0;
+    double l[2];
+    if (D > ZERO_TOLERANCE) {
+        n = 2;
+        l[0] = (-B + sqrt(D)) / (2 * A);
+        l[1] = (-B - sqrt(D)) / (2 * A);
+    } else if (D >= 0) {
+        n = 1;
+        l[0] = -B / (2 * A);
+    }
+    for (size_t i = 0; i < n; ++i) {
+        double x = a.x + cos(a.a) * l[i];
+        double y = a.y + sin(a.a) * l[i];
+        double z = atan2(y - b.y, x - b.x);
+        z = z < 0 ? z + PI * 2 : z;
+        if (l[i] >= 0 && l[i] <= a.l && z >= b.o && z <= b.o + b.a) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool collArcArc(Arc a, Arc b) {
+    double x[2], y[2];
+    size_t n = collCircleCircle((Circle){a.x, a.y, a.r}, (Circle){b.x, b.y, b.r}, x, y);
+    if (n == 0) {
+        return false;
+    } else if (n == (size_t)-1) {
+        return !(a.o > b.o + b.a || a.o + a.a < b.o);
+    } else {
+        for (size_t i = 0; i < n; ++i) {
+            double a1 = atan2(y[i] - a.y, x[i] - a.x);
+            double a2 = atan2(y[i] - b.y, x[i] - b.x);
+            a1 = a1 < 0 ? a1 + PI * 2 : a1;
+            a2 = a2 < 0 ? a2 + PI * 2 : a2;
+            if (a1 >= a.o && a1 <= a.o + a.a && a2 >= b.o && a2 <= b.o + b.a) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+static size_t collCircleCircle(Circle a, Circle b, double *x, double *y) {
+    double dx = b.x - a.x;
+    double dy = b.y - a.y;
+    if (fabs(dx) > ZERO_TOLERANCE && fabs(dx) > fabs(dy)) {
+        double xy = (a.y - b.y) / dx;
+        double x0 = (s(a.r) - s(b.r) - s(a.x) + s(b.x) - s(a.y) + s(b.y)) / dx / 2;
+        double A = s(xy) + 1;
+        double B = 2 * (xy * x0 - xy * a.x - a.y);
+        double C = s(x0) - 2 * x0 * a.x + s(a.x) + s(a.y) - s(a.r);
+        double D = s(B) - 4 * A * C;
+        if (D > ZERO_TOLERANCE) {
+            y[0] = (-B + sqrt(D)) / (2 * A);
+            x[0] = xy * y[0] + x0;
+            y[1] = (-B - sqrt(D)) / (2 * A);
+            x[1] = xy * y[1] + x0;
+            return 2;
+        } else if (D >= 0) {
+            y[0] = -B / (2 * A);
+            x[0] = xy * y[0] + x0;
+            return 1;
+        } else {
+            return 0;
+        }
+    } else if (fabs(dy) > ZERO_TOLERANCE) {
+        double yx = (a.x - b.x) / dy;
+        double y0 = (s(a.r) - s(b.r) - s(a.x) + s(b.x) - s(a.y) + s(b.y)) / dy / 2;
+        double A = s(yx) + 1;
+        double B = 2 * (yx * y0 - yx * a.y - a.x);
+        double C = s(y0) - 2 * y0 * a.y + s(a.x) + s(a.y) - s(a.r);
+        double D = s(B) - 4 * A * C;
+        if (D > ZERO_TOLERANCE) {
+            x[0] = (-B + sqrt(D)) / (2 * A);
+            y[0] = yx * x[0] + y0;
+            x[1] = (-B - sqrt(D)) / (2 * A);
+            y[1] = yx * x[1] + y0;
+            return 2;
+        } else if (D >= 0) {
+            x[0] = -B / (2 * A);
+            y[0] = yx * x[0] + y0;
+            return 1;
+        } else {
+            return 0;
+        }
+    } else if (fabs(a.r - b.r) < ZERO_TOLERANCE) {
+        return (size_t)-1;
+    } else {
+        return 0;
+    }
+}
+
+static double s(double x) {
+    return x * x;
+}
+
+static double ctg(double a) {
+    return cos(a) / sin(a);
+}
+
+static double len(double x1, double y1, double x2, double y2) {
+    return sqrt(s(x1 - x2) + s(y1 - y2));
 }
